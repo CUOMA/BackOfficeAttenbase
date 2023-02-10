ca9b98ffebadc8b13672c904fe8a223d93c36c09
Federico <gimanezf@gmail.com>
2023-02-10 13:49:57 -0300

    imports correction

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { QuicklinkModule } from 'ngx-quicklink';
 import { MaterialModule } from '../core/modules/material.module';
 import { ButtonComponent } from './button/button.component';
