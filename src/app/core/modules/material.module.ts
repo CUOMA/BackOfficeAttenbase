f2fabe0d7bbfedf6c6a52d859a4eee24439b947b
Federico <gimanezf@gmail.com>
2023-02-14 15:25:20 -0300

    add components mat module

--- src/app/core/modules/material.module.ts
+++ src/app/core/modules/material.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatListModule } from '@angular/material/list';
 import { MatInputModule } from '@angular/material/input';
 import { CustomMatIconsRegistratorModule } from './icons/custom-mat-icons-registrator.module';
 
@@ -12,6 +14,8 @@ import { CustomMatIconsRegistratorModule } from './icons/custom-mat-icons-regist
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
+    MatTabsModule,
+    MatListModule,
   ],
 })
 export class MaterialModule {}
