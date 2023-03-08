b8f75ea82f9d6e21b74b89b47247071bf7896941
Florencia Sosa <florencia@cuoma.com>
2023-03-08 18:28:45 +0100

    eliminar console.log

--- src/app/views/dashboard/aside/nav/nav.component.ts
+++ src/app/views/dashboard/aside/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Pages } from 'src/app/core/models/pages';
 
 @Component({
@@ -6,11 +6,9 @@ import { Pages } from 'src/app/core/models/pages';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
 })
-export class NavComponent implements OnInit {
+export class NavComponent  {
   @Input() isOpen!: boolean;
-  ngOnInit(): void {
-    console.log('');
-  }
+ 
   public pages: Pages[] = [
     { label: 'Preguntas', relativePath: 'listado-de-preguntas', icon: 'list-question', data: 356 },
     { label: 'Categorías', relativePath: 'categorias', icon: 'categories', data: 356 },
