35784bbdf0ff9da42b4d3a578e1384b464baa6d5
fedegime <fgimenez@cuoma.com>
2023-04-28 09:23:26 -0300

    routing fix

--- src/app/shared/back-arrow-button/back-arrow-button-routing.module.ts
+++ src/app/shared/back-arrow-button/back-arrow-button-routing.module.ts
@@ -1,16 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'redirect-listado-de-preguntas',
-    redirectTo: 'dashboard/listado-de-preguntas',
-    // loadChildren: () =>
-    //   import(
-    //     '../../views/dashboard/dashboard-views/dashboard-question/dashboard-question.module'
-    //   ).then(m => m.DashboardQuestionModule),
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
