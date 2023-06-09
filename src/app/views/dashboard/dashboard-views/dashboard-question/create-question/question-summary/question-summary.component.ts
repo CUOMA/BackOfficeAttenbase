4f4dbfd7572c1e241a4708499a8c66ecf7de6137
Florencia Sosa <florencia@cuoma.com>
2023-06-09 14:48:28 -0300

    remplazo de input alias por chips

--- src/app/views/dashboard/dashboard-views/dashboard-question/create-question/question-summary/question-summary.component.ts
+++ src/app/views/dashboard/dashboard-views/dashboard-question/create-question/question-summary/question-summary.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { DashboardCreateQuestionFacade } from '../dashboard-create-question.facade';
 
 @Component({
   selector: 'bdc-bo-question-summary',
   templateUrl: './question-summary.component.html',
   styleUrls: ['./question-summary.component.scss'],
 })
-export class QuestionSummaryComponent {}
+export class QuestionSummaryComponent implements OnInit {
+  protected preview!: any;
+  protected questionFacade = inject(DashboardCreateQuestionFacade);
+
+  ngOnInit(): void {
+    this.preview = this.questionFacade.formQuestion;
+    console.log(this.questionFacade.formQuestion);
+  }
+}
