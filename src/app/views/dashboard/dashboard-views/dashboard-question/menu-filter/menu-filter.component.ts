084d3739d150e1105ddabaaa9180efaad1fde210
fedegime <fgimenez@cuoma.com>
2023-06-12 17:13:08 -0300

    add styles to question filter menu and functionality close menu

--- src/app/views/dashboard/dashboard-views/dashboard-question/menu-filter/menu-filter.component.ts
+++ src/app/views/dashboard/dashboard-views/dashboard-question/menu-filter/menu-filter.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'bdc-bo-menu-filter',
@@ -6,4 +6,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./menu-filter.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MenuFilterComponent {}
+export class MenuFilterComponent {
+  @Output() closeMenuEmmiter = new EventEmitter();
+
+  protected deleteFilters() {
+    this.closeMenuEmmiter.emit();
+  }
+  protected expression = true;
+}
