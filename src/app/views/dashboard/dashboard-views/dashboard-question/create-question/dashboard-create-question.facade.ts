913306a6cdbeadfaeb14e1e9c61fd6966523423b
Florencia Sosa <florencia@cuoma.com>
2023-07-04 09:17:49 -0300

    avances

--- src/app/views/dashboard/dashboard-views/dashboard-question/create-question/dashboard-create-question.facade.ts
+++ src/app/views/dashboard/dashboard-views/dashboard-question/create-question/dashboard-create-question.facade.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { categoriesApiActions } from 'src/app/store/actions/categories.actions';
@@ -12,31 +12,39 @@ import {
   providedIn: 'root',
 })
 export class DashboardCreateQuestionFacade {
-  public titleQuestion = new BehaviorSubject<string>('Crear pregunta');
-
   public formQuestion?: FormQuestion;
+  private createQuestionTitle$ = new BehaviorSubject('Crear Pregunta');
 
   constructor(private store: Store) {}
+
+  public get createQuestionTitle(): Observable<string> {
+    console.log();
+    return this.createQuestionTitle$.asObservable();
+  }
 
   public dispatchGetListCategories(): void {
     this.store.dispatch(categoriesApiActions.getListCategoriesRequest());
   }
+
   public dispatchGetListSubcategories(id: number): void {
     this.store.dispatch(categoriesApiActions.getListSubcategoriesRequest({ id }));
   }
+
   public selectListCategories(): Observable<any> {
     return this.store.select(selectListCategories);
   }
+
   public selectListSubcategories(): Observable<any> {
     return this.store.select(selectListSubCategories);
   }
+
   public get areSubcategoriesLoading(): Observable<boolean> {
     return this.store.select(selectAreListSubcategoriesLoading);
   }
 
   public formMetadaQuestion(formData?: any, res?: any) {
-    this.titleQuestion.next(formData.question);
     this.formQuestion = { ...this.formQuestion, ...formData, ...res };
+    localStorage.setItem('datosFormulario', JSON.stringify(this.formQuestion));
   }
 }
 
